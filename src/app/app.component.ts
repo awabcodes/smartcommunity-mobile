26b2288226dcd0c0082f1c0e358a1806e41cd4d8
awabcodes <awabcodes@gmail.com>
2021-01-08 14:43:09 +0200

    side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Plugins } from '@capacitor/core';
-import { Platform } from '@ionic/angular';
+import { MenuController, NavController, Platform } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
+import { LoginService } from './services/login/login.service';
 
 const { SplashScreen } = Plugins;
 
@@ -10,7 +11,13 @@ const { SplashScreen } = Plugins;
   templateUrl: 'app.component.html',
 })
 export class AppComponent {
-  constructor(private platform: Platform, private translate: TranslateService) {
+  constructor(
+    private platform: Platform,
+    private translate: TranslateService,
+    private loginService: LoginService,
+    private menu: MenuController,
+    private navController: NavController
+  ) {
     this.initializeApp();
   }
 
@@ -34,5 +41,11 @@ export class AppComponent {
     // this.translate.get(['BACK_BUTTON_TEXT']).subscribe(values => {
     //   this.config.set('ios', 'backButtonText', values.BACK_BUTTON_TEXT);
     // });
+  }
+
+  logout() {
+    this.loginService.logout();
+    this.menu.close();
+    this.navController.navigateBack('');
   }
 }
